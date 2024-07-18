46621c582778797f0cc235b76606d586d9e4b821
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-07-18 20:25:21 +0200

    Current home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,126 @@
-import Image from "next/image";
+// page.tsx
 
-export default function Home() {
+"use client";
+
+import s from './page.module.css'
+
+const Home = () => {
+    const newPopup = (url: string) => {
+            window.open(
+                url,
+                "popUpWindow",
+                "height=800,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes"
+        );
+    };
+
+    const inviteRedirect = () => {
+        window.open(
+            "https://discord.com/oauth2/authorize?client_id=701280304182067251&permissions=412317141056&scope=applications.commands%20bot"
+        );
+    };
+
+    const githubRedirect = () => {
+        window.open("https://github.com/Yetity/y2b");
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        if (event.key !== "y") {
+            return;
+        }
+        const answer = prompt(
+            "Enter code for free bobux (entering wrong codes will result in consequences)"
+        );
+    // made the answer actually claimable lol
+        if (answer !== "yetbot.tk") {
+            window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0");
+            return;
+        }
+
+        alert("Claim this promocode(remove the square brackets and no spaces): [webæ¹¥¶¿áá³¤ë_!] ");
+    };
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">\
-            <p>Whats the fuck</p>
-        </main>
-  );
-}
+    <main className={s.main}>
+        <h1 className={s.mainTitle}>
+            Become the richest user in your server today, with Yet 2.0
+        </h1>
+        <div className={s.buttons}>
+            <button type="button" className="btn btnStyle" onClick={inviteRedirect}> Invite </button>
+        <button type="button" className="btn btnStyle" onClick={githubRedirect}> Github</button>
+        </div>
+        <picture><img src="coinz.png" alt="coin" id="coin" className="coin" width={100} height={100} onContextMenu={(e) => e.preventDefault()} /></picture>
+        <div className={s.features}>
+            <div className={s.feature}>
+                <picture><img src="features/work.png" alt="a pic" /></picture>
+                <span>Work jobs for coins,</span>
+            </div>
+            <div className={s.feature}>
+                <picture><img src="features/rob.png" alt="a pic" /></picture>
+                <span>Rob other users for their coins,</span>
+            </div>
+            <div className={s.feature}>
+                <picture><img src="features/shar4e.png" alt="a pic" /></picture>
+                <span>Share your coins to those in need,</span>
+            </div>
+            <div className={s.feature}>
+                <picture><img src="features/shurg.png" alt="a pic" /></picture>
+                <span>..or just casually climb the leaderboard with no worries.</span>
+            </div>
+            <div className={s.feature}>
+                <picture><img src="features/upcoming.png" alt="a pic" /></picture>
+                <span>and (soon to be) many more!</span>
+            </div>
+        </div>
+        <span id="goDoIt">
+            So what you waiting for? Go
+        <a
+            href="https://discord.com/oauth2/authorize?client_id=701280304182067251&permissions=412317141056&scope=applications.commands%20bot"
+            id="inviteRed"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            invite
+        </a>
+        the bot right now
+        </span>
+        <div id="links">
+            <div className="buttons">
+                <button type="button" className="btn btnStyle" onClick={() => { window.location.href = '/tos/'; }}>
+                    Terms Of Service
+                </button>
+            </div>
+        </div>
+        <div className="container">
+            <div id="credits">
+                <div className="user">
+                    <picture><img className="creditsImg" src="credits/yet.jpeg" alt="Yet"/></picture>
+                    <div className="creditsContent">
+                        <div className="contri">Bot Owner/Creator, HTML, CSS, Animations, Images</div>
+                        <br />
+                        <div className="userLinks">
+                            <a href="https://yetnt.pages.dev">Website</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="user">
+                    <picture><img className="creditsImg" src="credits/flo.png" alt="Flo" /></picture>
+                    <div className="creditsContent">
+                        <div className="contri">ToS, Ideas,</div>
+                        <br />
+                        <div className="userLinks">
+                            <a href="https://devflo.pages.dev">Website</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div id="copyright">
+                &copy;2023 Yet 2.0 Bot. All rights reserved.
+            </div>
+        </footer>
+    </main>
+    );
+};
+
+export default Home;
