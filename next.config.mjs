59dc33b52b391bc21eab43fcef5f14f0d821b347
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-07-20 18:27:13 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async exportPathMap(defaultPathMap) {
-        // Filter out the API routes
-        const filteredPathMap = Object.keys(defaultPathMap)
-            .filter((path) => !path.startsWith("/api"))
-            .reduce((acc, path) => {
-                acc[path] = defaultPathMap[path];
-                return acc;
-            }, {});
-
-        return filteredPathMap;
-    },
+    output: "standalone",
 };
 
 export default nextConfig;
