3def52b1104729a70336fb9220507129169cc636
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-10-08 16:58:48 +0200

    this is the documenttion

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -1,23 +1,26 @@
 // import { Document } from "mongoose";
+
+/** Type it should be from the DB */
 export interface ISubcommand {
     name: string;
     description: string;
     use: string;
 }
-
+/** Type it should be from the DB */
 export interface ICommand {
     name: string;
     description?: string;
     use?: string;
     subcommands?: ISubcommand[];
 }
-
+/** API Type it should be from the DB */
 export interface ApiSubcommand {
     name: string;
     description: string;
     use: string;
 }
 
+/** API Type it should be from the DB */
 export interface ApiCommand {
     name: string;
     description?: string;
